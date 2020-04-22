e89e99d6f3c3691660222949b9a779e9fadac701
scott <scottakirschner@gmail.com>
2020-04-21 22:41:13 -0400

    pushing project

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = '6p81HZ3s-W_DAk_5M2ZmE9HoaLyajJssxz6aoIfXeh0oxwGnyGkpLBUYAHlzGSduEP2DRNR0raMyaZy68uApLXACDDBIlOCkReAWXUnDYrQdgCXb4_GRFfWwsJafXnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
